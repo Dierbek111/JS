ced63c375339645a89f8979b2873d966744ecb95
Dierbek111 <dierbekrahmatullaev00@gmail.com>
2024-07-26 19:35:18 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+let a = prompt("How old are you?")
+
+if (a >= 18){
+    console.log("вы совершеннолетний")
+}
+else if(a < 0 ){
+    console.log("вы еще не в планах");
+}
+ else if (a <= 18) {
+    console.log("вы несовершеннолетний")
+} 
+else if (a <= 0) {
+    console.log("вы не родились")
+}
+
